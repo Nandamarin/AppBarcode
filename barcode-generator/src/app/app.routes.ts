36d2e8e85f20fbe030279fd0bc876eef04e82d93
Nandamarin <nanda.marin.mec@gmail.com>
2024-10-03 17:04:49 -0300

    tela de login criada

--- barcode-generator/src/app/app.routes.ts
+++ barcode-generator/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { BarcodeGeneratorComponent } from './barcode-generator/barcode-generator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent }, // Rota padrão
+  { path: 'generate-barcode', component: BarcodeGeneratorComponent } // Rota para gerar código de barras
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
