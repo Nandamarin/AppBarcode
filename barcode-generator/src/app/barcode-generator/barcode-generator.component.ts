0b9ae18e4438eb8784d7ce335150082afee833e5
Nandamarin <nanda.marin.mec@gmail.com>
2024-10-03 13:41:20 -0300

    criado os componentes login e barcode-generator

---
+++ barcode-generator/src/app/barcode-generator/barcode-generator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barcode-generator',
+  standalone: true,
+  imports: [],
+  templateUrl: './barcode-generator.component.html',
+  styleUrl: './barcode-generator.component.scss'
+})
+export class BarcodeGeneratorComponent {
+
+}
